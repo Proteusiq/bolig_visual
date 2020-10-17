2a26ae9c0ee99680cb1af9bbe4a8784cbd62719e
Prayson W. Daniel <praysonwilfred@gmail.com>
2020-10-17 15:56:19 +0200

    ready to populate data with python

---
+++ containers/grafana/dashboard/datasource.js
@@ -0,0 +1,15 @@
+{
+    "name": "postgres-datasource",
+    "type": "postgres",
+    "typeLogoUrl": "public/app/plugins/datasource/postgres/img/postgresql_logo.svg",
+    "access": "proxy",
+    "url": "postgres:5432",
+    "password": "grafanareader",
+    "user": "grafanareaderpwd",
+    "database": "boligDB",
+    "basicAuth": false,
+    "isDefault": true,
+    "jsonData": {
+            "sslmode": "disable"
+    }
+}
