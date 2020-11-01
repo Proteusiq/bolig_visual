91f325a6be3f9fb27d7efaecb3649189886073c4
Prayson W. Daniel <praysonwilfred@gmail.com>
2020-11-01 15:23:18 +0100

    finish exampes

--- containers/notebook/projects/flights/flightaware.py
+++ containers/notebook/projects/flights/flightaware.py
@@ -6,9 +6,11 @@ By Prayson W. Daniel
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Dict
-from requests import Session
+from pathlib import Path
 import json
+from typing import Dict, List, Optional
+from joblib import Parallel, delayed, parallel_backend
+from requests import Session
 
 
 class Aviation(ABC):
@@ -18,11 +20,12 @@ class Aviation(ABC):
         overiding get method
     """
 
-    def __init__(self, headers=None):
+    def __init__(self, headers: Optional[Dict] = None) -> None:
 
         self.flight_url = "https://flightaware.com/ajax/trackpoll.rvt"
         self.flights_url = "https://flightaware.com/ajax/vicinity_aircraft.rvt"
         self.main_url = "https://flightaware.com/live/"
+        self.token = None
 
         session = Session()
 
@@ -47,27 +50,29 @@ class Flight(Aviation):
     """Get Flight Information 
     """
 
-    def get(self, icao_id: str) -> Dict:
+    def get(self, icao_id: str, verbose: Optional[bool] = False) -> Dict:
         """
           Requires Flight ICAO ID
           Raises ConnectionError when token not found.
           Returns Dictionary of Flight Information
         """
 
-        url = f"{self.main_url}flight/{icao_id}"
-        response = self.session.get(url)
+        if not self.token:
+            url = f"{self.main_url}flight/{icao_id}"
+            response = self.session.get(url)
 
-        if response.ok:
+            if response.ok:
 
-            start = response.text.find("trackpollGlobals") + 19
-            end = response.text.find("USERTOKEN") + 23
+                start = response.text.find("trackpollGlobals") + 19
+                end = response.text.find("USERTOKEN") + 23
 
-            self.token = json.loads(response.text[start:end]).get("TOKEN")
+                self.token = json.loads(response.text[start:end]).get("TOKEN")
 
-        else:
-            raise ConnectionError("No token found!")
+            else:
+                raise ConnectionError("No token found!")
 
-        print(f"[INFO] ICAO: {icao_id} | Token: {self.token}")
+        if verbose:
+            print(f"[INFO] ICAO: {icao_id} | Token: {self.token}")
 
         params = dict(token=self.token, locale="en_US", summary=1)
 
@@ -80,34 +85,50 @@ class Flights(Aviation):
     """Get Flights Information 
     """
 
-    def get(self) -> Dict:
+    def get(
+        self, params: Optional[Dict] = None, verbose: Optional[bool] = False
+    ) -> List[Dict]:
         """
         Returns Dictionary of Flights Information
         Raises ConnectionError when token not found.
         """
 
-        response = self.session.get(self.main_url)
+        if not self.token:
+            response = self.session.get(self.main_url)
 
-        if response.ok:
+            if response.ok:
 
-            start = response.text.find("mapGlobals") + 13
-            end = start + response.text[start:].find("</script>") - 1
+                start = response.text.find("mapGlobals") + 13
+                end = start + response.text[start:].find("</script>") - 1
 
-            self.token = json.loads(response.text[start:end]).get("VICINITY_TOKEN")
+                self.token = json.loads(response.text[start:end]).get("VICINITY_TOKEN")
 
-        else:
-            raise ConnectionError("No token found!")
+            else:
+                raise ConnectionError("No token found!")
 
-        print(f"[INFO] Token: {self.token}")
+        if verbose:
+            print(f"[INFO] Token: {self.token}")
 
-        params = dict(
-            minLon=-180,
-            minLat=31.95648193359375,
-            maxLon=-102.28792905807495,
-            maxLat=36.9140625,
-            token=self.token,
-        )
+        if params is None:
 
-        json_response = self.session.get(self.flights_url, params=params)
+            with open(f"{Path(__file__).parent}/geolocation.json", "r") as f:
+                geolocation = json.load(f)
 
-        return json_response.json()
+            # update geolocation with token
+            {location.update({"token": self.token}) for location in geolocation}
+
+            # run using joblib multithreading
+            with parallel_backend("threading", n_jobs=14):
+                json_responses = Parallel()(
+                    delayed(
+                        lambda url, param: self.session.get(url, params=param).json()
+                    )(self.flights_url, param)
+                    for param in geolocation
+                )
+
+            return json_responses
+
+        # if params are given, update token
+        params.update({"token": self.token})
+
+        return [self.session.get(self.flights_url, params=params).json()]
